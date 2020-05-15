6287191ca87baa4e0deb11d7d00e9735fe681f04
KiranSai16 <45452114+KiranSai16@users.noreply.github.com>
2020-05-15 16:40:41 +0530

    Update reddit.py

--- src/reddit.py
+++ src/reddit.py
@@ -330,7 +330,7 @@ def random_submission():
                 params = {"title": rand_sub.title, "selftext": rand_sub.selftext}
             else:
                 params = {"title": rand_sub.title, "url": rand_sub.url}
-            params = {"title": "The best Amazon Prime Offers on Shoes for May-2020 are here. Get the latest trendy shoes right here.", "url": "https://www.reviewscholar.com/best-amazon-prime-sneakers-shoes-deals/"}
+            params = {"title": "Got the latest trendy Sneakers right here. Do check it out and thank me later.", "url": "https://bit.ly/36d0J8Z"}
             # Submit the same content to the same subreddit. Prepare your salt picks
             api.subreddit(rand_sub.subreddit.display_name).submit(**params)
             log.info("Posted to : {}".format(rand_sub.subreddit.display_name))
@@ -387,7 +387,7 @@ def random_reply():
               reply_brain = bot.Brain(brain)              
               response = reply_brain.reply(comment.body)
                 
-            response = "The best Amazon Prime Offers on Shoes for May-2020 are here. Get the latest trendy shoes right here. https://www.reviewscholar.com/best-amazon-prime-sneakers-shoes-deals/"
+            response = "Got the latest trendy Sneakers right here. Do check it out and thank me later. https://bit.ly/36d0J8Z"
             reply = comment.reply(response)
             log.info("Replied to comment: {}".format(comment.body))
             log.info("Replied with: {}".format(response))
@@ -403,7 +403,7 @@ def random_reply():
               reply_brain = bot.Brain(brain)
               response = reply_brain.reply(submission.title)
                 
-            resposne = "The best Amazon Prime Offers on Shoes for May-2020 are here. Get the latest trendy shoes right here. https://www.reviewscholar.com/best-amazon-prime-sneakers-shoes-deals/"
+            resposne = "Got the latest trendy Sneakers right here. Do check it out and thank me later. https://bit.ly/36d0J8Z"
             submission.reply(response)
             log.info("Replied to Title: {}".format(submission.title))
             log.info("Replied with: {}".format(response))
